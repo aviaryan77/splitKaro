4a834d8696b1327b61ff40aeaea4154b19b0a2ca
aviaryan77 <aviaryan77@gmail.com>
2022-05-13 03:01:32 +0530

    removed extra comments

--- App.js
+++ App.js
@@ -1,32 +1,3 @@
-// Hello there!
-
-// Splitkaro is a group expense management app where you can split, track & settle with friends, expenses done in groups. We are different from others in the market (How? You can download the app & check for yourself or visit our Instagram/LinkedIn profile & check the post on our differentiators)
-
-// Problem Statement
-// Now, a challenge people face is → “We are already using another app. How do we start using yours if all our balances are maintained there?”
-// Our answer to this - “Just like how we change our bank passbook, when the all the pages are filled. We start the new passbook with the last balance record.”
-
-// Assignment
-// Assuming a group needs to start recording their expenses on Splitkaro now after using some other app/way to record their group expenditure/
-// We would love to see how you do it in React Native (Expo or even standalone)
-
-// Requirements
-
-// An entry point “Add Existing Group Balance” (You can choose any other way/page to show this)
-// On click, opens “Create a group” page/overlay
-// Enter group name
-// Group Type → Home/Trip/Office, etc
-// Add members from Contact & using phone numbers
-// Add Existing Balances (explained in detail below)
-// Create Group button
-// Add Existing Balance → A person can either owe or be owed an amount in a group. So, his/her balance could be +ve or -ve. So you must have a way to enter +/- balances.
-// +ve → Is owed (Will get money)
-// -ve → owes (will give money)
-// Create Group Button takes the user to the Group Page that displays the balances of the group members.
-
-// We expect you to complete this assignment & submit it within 5 days with the link to working sample or a github code.
-// Happy Coding :)
-// In case of any query or concern please write to avinash@splitkaro.com
 import { LinearGradient } from 'expo-linear-gradient';
 import { ScrollView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
